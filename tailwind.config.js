ae7f8a8ef5f68ac8100bac345101e890c4120b37
Kenneth Buan <buancneth@gmail.com>
2019-08-16 05:41:57 +0800

    uploading files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors:{
+        'brand-blue': '#1992d4',
+      },
+    }
+  },
+  variants: {
+    backgroundColor: ['responsive','hover', 'focus','active']
+  },
+  plugins: []
+}
